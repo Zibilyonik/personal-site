7900973c6aa0bbbed9f575398175ccdfded5981b
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-13 15:11:36 +0200

    Swap image url with placeholder

--- src/components/projects_db.js
+++ src/components/projects_db.js
@@ -1,23 +1,21 @@
+import logo from "../logo.svg";
 export const projects = [
     {
         id: 1,
-        image: 'https://example.com/images/project1.jpg',
+        image: logo,
         title: 'Project 1',
         description: 'This is the description for Project 1.',
-        url: 'https://example.com/projects/1',
     },
     {
         id: 2,
-        image: 'https://example.com/images/project2.jpg',
+        image: logo,
         title: 'Project 2',
         description: 'This is the description for Project 2.',
-        url: 'https://example.com/projects/2',
     },
     {
         id: 3,
-        image: 'https://example.com/images/project3.jpg',
+        image: logo,
         title: 'Project 3',
         description: 'This is the description for Project 3.',
-        url: 'https://example.com/projects/3',
     },
 ];
