4f08483810ebd6299991712f950c9bd6b24cd262
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-13 15:21:52 +0200

    add BrowserRouter and Projects comp call

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="App-header">
-          <Hero />    
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="App-header">
+            <Hero />    
+          </div>
+          <Projects />
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
