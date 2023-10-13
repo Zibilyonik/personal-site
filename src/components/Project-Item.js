25f08e434ffdcc48c67d2a4f6b9025e3074326b9
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-13 15:18:06 +0200

    remove a element and add classes

--- src/components/Project-Item.js
+++ src/components/Project-Item.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import "../assets/scss/project-item.scss"
 
 function ProjectItem(props) {
     return (
-        <div>
-            <a href={props.url}>
-                <img src={props.image} alt={props.title} />
-                <h2>{props.title}</h2>
-            </a>
-            <p>{props.description}</p>
+        <div className="project-item">
+            <img className="project-img" src={props.image} alt={props.title} />
+            <h2 className="project-title">{props.title}</h2>
+            <p className="project-desc">{props.description}</p>
         </div>
     );
 }
