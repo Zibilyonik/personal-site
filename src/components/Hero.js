03062717209fd4583e5db3c2b242d33525cb2065
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-20 01:05:28 +0200

    adjust code to escape quotation mark

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
     <div className="hero-container">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="styled-text-cont">
-            <h1 className="title"><span className="coloured">Hi!</span> I'm Martyna - a designer focused on UX/UI design, visual design and prototyping.</h1>
+            <h1 className="title"><span className="coloured">Hi!</span> {"I'm Martyna - a designer focused on UX/UI design, visual design and prototyping."}</h1>
         </div>
     </div>
     )
