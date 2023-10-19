a269fb594260872456e013605f50bd6eb6c9d02b
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-20 01:06:40 +0200

    replace react.render with createroot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+if (root) {
+  const rootElement = ReactDOM.createRoot(root);
+  rootElement.render(<App />);
+}
