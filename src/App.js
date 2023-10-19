2e074c617c995ac567fabb0747a376b4e54784c0
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-20 01:08:31 +0200

    refactor App component to acommodate createroot, add routes and about component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
+import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <div className="App-header">
-            <Hero />    
-          </div>
-          <Projects />
-        </div>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Hero />
+        <Routes>
+          <Route path="/" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
