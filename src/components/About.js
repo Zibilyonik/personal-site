05bb5ef6aac21b53fee2a1ccc1e5f449aa25b530
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-10-20 01:05:56 +0200

    add template about page component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from './Hero';
+
+function About() {
+    return (
+        <div>
+            <Hero />
+            <p>Hi, my name is [Your Name] and I&rsquo;m a [Your Profession].</p>
+            <p>I have [Number of Years] years of experience in [Your Field] and have worked on projects such as [Project 1], [Project 2], and [Project 3].</p>
+            <p>In my free time, I enjoy [Hobby 1], [Hobby 2], and [Hobby 3].</p>
+        </div>
+    );
+}
+
+export default About;
