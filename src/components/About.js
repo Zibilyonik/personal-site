6da1d444d60c4ad533567773c9677355b988a73e
Onur <onatsurmeli@gmail.com>
2023-10-28 03:55:31 +0200

    add div class

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,10 @@ import { projects } from './projects_db';
 
 function About() {
     return (
-        <div>
-            <p>Hi, my name is Martyna and I&rsquo;m a UX/UI designer.</p>
-            <p>I have [Number of Years] years of experience in UX/UI Design and have recently worked on projects such as {projects[0].title}, {projects[1].title}, and {projects[2].title}.</p>
-            <p>In my free time, I enjoy [Hobby 1], [Hobby 2], and [Hobby 3].</p>
+        <div className='styled-text-cont'>
+            <h1>Hi, my name is Martyna and I&rsquo;m a UX/UI designer.</h1>
+            <h1>I have [Number of Years] years of experience in UX/UI Design and have recently worked on projects such as {projects[0].title}, {projects[1].title}, and {projects[2].title}.</h1>
+            <h1>In my free time, I enjoy [Hobby 1], [Hobby 2], and [Hobby 3].</h1>
         </div>
     );
 }
