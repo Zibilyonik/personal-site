c1726cb11b449daa5bab6517f7ff0674a1f94ed5
Onur <onatsurmeli@gmail.com>
2023-10-28 03:26:44 +0200

    create Home component to combine Hero and Projects

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./Hero";
+import Projects from "./Projects";
+
+function Home() {
+    return (
+        <div>
+            <Hero />
+            <Projects />
+        </div>
+    );
+}
+
+export default Home;
