4b011a356786670a8872265f761325e8692f727c
Onur <onatsurmeli@gmail.com>
2023-10-28 03:27:03 +0200

    add db import to dynamically adjust project titles

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Hero from './Hero';
+import { projects } from './projects_db';
 
 function About() {
     return (
         <div>
-            <Hero />
-            <p>Hi, my name is [Your Name] and I&rsquo;m a [Your Profession].</p>
-            <p>I have [Number of Years] years of experience in [Your Field] and have worked on projects such as [Project 1], [Project 2], and [Project 3].</p>
+            <p>Hi, my name is Martyna and I&rsquo;m a UX/UI designer.</p>
+            <p>I have [Number of Years] years of experience in UX/UI Design and have recently worked on projects such as {projects[0].title}, {projects[1].title}, and {projects[2].title}.</p>
             <p>In my free time, I enjoy [Hobby 1], [Hobby 2], and [Hobby 3].</p>
         </div>
     );
