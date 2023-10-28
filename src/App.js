ad81f87dbb94e8590c0fd6cde8a386f350908175
Onur <onatsurmeli@gmail.com>
2023-10-28 03:27:47 +0200

    Change Hero and Projects components calls to Home

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Navbar from './components/navbar';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
+import Home from './components/Home';
 import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,9 +11,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Hero />
         <Routes>
-          <Route path="/" element={<Projects />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
