57aebb9c82d0101815421270c3449418686249bb
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-11-03 17:09:27 +0100

    add a button to view resume

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ import { projects } from './projects_db';
 function About() {
     return (
         <div className='styled-text-cont'>
+            <a href="./Resume Martyna Szczepaniak.pdf" target='_blank'>
+                <button type="button">Download My Resume</button>
+            </a>
             <h1>Hi, my name is Martyna and I&rsquo;m a UX/UI designer.</h1>
             <h1>I have [Number of Years] years of experience in UX/UI Design and have recently worked on projects such as {projects[0].title}, {projects[1].title}, and {projects[2].title}.</h1>
             <h1>In my free time, I enjoy [Hobby 1], [Hobby 2], and [Hobby 3].</h1>
