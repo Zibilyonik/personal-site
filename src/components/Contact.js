361b8d20f5b0ba2fbcb4aacb5b1a644094d77e2c
Onur <onatsurmeli@gmail.com>
2023-11-06 13:28:46 +0100

    import scss into component, change links to h2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../assets/scss/Contact.scss';
 const Contact = () => {
     // const handleSubmit = (event) => {
     //     event.preventDefault();
@@ -7,12 +7,12 @@ const Contact = () => {
     // };
 
     return (
-        <div>
+        <div id="Contact">
             <h1>Let&rsquo;s Connect!</h1>
-            <h1><a href="mailto:mszczepaniakux@gmail.com" target="_blank" rel="noreferrer">Email</a></h1>
-            <h1><a href="https://www.linkedin.com/in/martyna-szczepaniak-ux" target="_blank" rel="noreferrer">LinkedIn</a></h1>
-            <h1><a href="https://wa.me/+48500136023" target="_blank" rel="noreferrer">WhatsApp</a></h1>
-            <h1><a href="https://www.behance.net/martynaszczepa1" target="_blank" rel="noreferrer">Behance</a></h1>
+            <h2><a href="mailto:mszczepaniakux@gmail.com" target="_blank" rel="noreferrer">Email</a></h2>
+            <h2><a href="https://www.linkedin.com/in/martyna-szczepaniak-ux" target="_blank" rel="noreferrer">LinkedIn</a></h2>
+            <h2><a href="https://wa.me/+48500136023" target="_blank" rel="noreferrer">WhatsApp</a></h2>
+            <h2><a href="https://www.behance.net/martynaszczepa1" target="_blank" rel="noreferrer">Behance</a></h2>
             {/* <form onSubmit={handleSubmit}>
                 <input type="text" name="name" placeholder="Your Name" required />
                 <input type="email" name="email" placeholder="Your Email" required />
