4437ec13dc170a89d76746a538851e10fde07812
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-11-07 21:49:31 +0100

    add placeholder project components and routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Project1 from './components/Projects/Project1';
+import Project2 from './components/Projects/Project2';
+import Project3 from './components/Projects/Project3';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -16,6 +20,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/project-1" element={<Project1 />} />
+          <Route path="/projects/project-2" element={<Project2 />} />
+          <Route path="/projects/project-3" element={<Project3 />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/components/Projects/Project3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project3() {
+  return (
+    <div>
+      <h1>Yes, hello, here is Project3!</h1>
+    </div>
+  );
+}
+
+export default Project3;
