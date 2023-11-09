c461be26cd9bf9a8fd4cd44fd9722d1369bee378
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-11-09 19:30:04 +0100

    add optional subtitle element for project pages

--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 function TitleCard(props) {
     return (
         <div className='title-card'>
-            <img src={props.path} alt={props.name} />
-            <h1>{props.name}</h1>
+            <img className="titlecard-img" src={props.path} alt={props.name} />
+            <h1 className={`titlecard-title ${!props.subtitle ? "titlecard-last" : ""}`}>{props.name}</h1>
+            {props.subtitle ? <h2 className="titlecard-subtitle titlecard-last">{props.subtitle}</h2> : null}
         </div>
     );
 }
 
 TitleCard.propTypes = {
     name: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
     path: PropTypes.string.isRequired,
 };
 
