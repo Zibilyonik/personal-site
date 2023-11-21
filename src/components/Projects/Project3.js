0b838602655e242adf1a3a2b66a1590f2ce4fe14
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-11-21 22:03:43 +0100

    finish 3rd project content

--- src/components/Projects/Project3.js
+++ src/components/Projects/Project3.js
@@ -25,31 +25,32 @@ function Project3() {
             <p><span>Potential Features:</span> QR pairing, forum, friends list, parental control</p>
           </section>
           <section>
+            <h3>User Research Painpoints</h3>
+            <p><span>Time:</span> Studying young adults are to busy to keep track of their score on their own</p>
+            <p><span>Child Friendly Features:</span> Score tracking apps don’t focus on implementing child friendly features</p>
+            <p><span>Accessibility:</span> Platforms for score tracking are not equipped with assistive technologies</p>
+            <p><span>Design:</span> Most of the Arcade related apps have dated, not user friendly design</p>
+          </section>
+          <section>
             <h3>Meet the Users</h3>
             <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-            <p>Borys is a 47-years old freelance photographer, who needs to find someone to work with, because he’s not tech savvy and wants to focus on the things he knows and is good at.</p>
+            <p>Felix is a 21-year-old student who needs an app that allows him to share and compare high scores because he spends a lot of time playing with his friends and preparing for a tournament.</p>
+            <h3>User Journey Map</h3>
             <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-            <p>Ola is a 26-years-old fresh photography major, who needs a way to promote her business, because she wants to get more clients and commissions.</p>
           </section>
         </div>
         <div className="project-info">
           <h2>Initial Design Process</h2>
           <section>
-            <h3>Sitemap</h3>
-            <p>Difficulty with website navigation was a primary pain point for users, so I used that knowledge to create a sitemap.</p>
-            <p>My goal here was to make strategic information architecture decisions that would improve overall website navigation. The structure I chose was designed to make things simple and easy.</p>
-            <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-          </section>
-          <section>
             <h3>Paper Wireframes</h3>
-            <p>Next, I sketched out paper wireframes for each screen in my app, keeping the user pain points about navigation, browsing, and checkout flow in mind.</p>
+            <p>Taking the time to draft iterations of each screen of the app on paper ensured that the elements that made it to digital wireframes would be well-suited to address user pain points. For the home screen, I prioritized a quickly and easily accessible scan button to help users save time.</p>
+            <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
             <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
           </section>
           <section>
             <h3>Digital Wireframes</h3>
             <p>As the initial design phase continued, I made sure to base screen designs on feedback and findings from the user research.</p>
-            <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-            <p>Using the completed set of digital wireframes, I created a low-fidelity prototype. The primary user flow I connected was creating the account and posting a job offer, so the prototype could be used in a usability study.</p>
+            <p>Using the completed set of digital wireframes, I created a low-fidelity prototype. The primary user flow I connected was connecting the device to an arcade machine and finding the opponent, so the prototype could be used in a usability study.</p>
             <img className="img-rec" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
           </section>
         </div>
@@ -58,30 +59,23 @@ function Project3() {
           <section>
             <p><span>Study type:</span> Unmoderated usability study</p>
             <p><span>Location:</span> Wroclaw, remote</p>
-            <p><span>Participants:</span> 5 participants, 3 male, 2 female, ages 18-53</p>
-            <p><span>Length:</span> 20-30 minutes per session</p>
+            <p><span>Participants:</span> 5 participants, 3 male, 2 female, ages 6-45</p>
+            <p><span>Length:</span> 15-25 minutes per session</p>
           </section>
           <section>
             <h3>Usability Study Findings</h3>
-            <p><span>Salary Range:</span> Users would like to have a salary range included in the job offer</p>
-            <p><span>Short Description:</span> Users would like to have short descriptions in their profiles</p>
-            <p><span>Skills:</span> Users would prefer skills to be listed with ratings</p>
-            <p><span>Gender:</span> Users don&rsquo;t want a gender option in profile creation</p>
+            <p><span>Scanner:</span> The QR scanner button should be more visible</p>
+            <p><span>Labeling:</span> Connecting to other users is confusing</p>
+            <p><span>Manual Score Input:</span> The process of manually inputting score should be simplified</p>
           </section>
         </div>
         <div className="project-info">
           <h2>Refining the Design</h2>
-          <p>Early designs allowed for some customization, but after the usability studies, I added additional options to go back to sign in window. I also revised the design so users don’t have to choose their gender when they first create their profile.</p>
+          <p>Early designs allowed for connecting to the other users, but after the usability studies, I switched the “invite” button to “send message” to give users an option to talk to the potential opponent fist. I&rsquo;ve also revised the design so users see QR code scanner better when they first land on the home screen.</p>
           <h3>Before</h3>
           <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
           <h3>After</h3>
           <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-          <p>The skill listing has been changed into tags system that allows rating of each of them.</p>
-          <h3>Before</h3>
-          <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-          <h3>After</h3>
-          <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
-          <p>Additional design changes included an option of adding a salary range to a job post.</p>
           <h3>Before</h3>
           <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
           <h3>After</h3>
@@ -89,19 +83,19 @@ function Project3() {
         </div>
         <div className="project-info">
           <h2>High Fidelity Prototype</h2>
-          <p>The high-fidelity prototype followed the same user flow as the low-fidelity prototype, including design changes made after the usability study.</p>
-          <a><p>View ClickedIn’s high fidelity prototype</p></a>
+          <p>The final high-fidelity prototype presented cleaner user flows for pairing the device and connecting to other users.</p>
+          <a><p>View GameMaster’s high fidelity prototype</p></a>
           <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
           <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
           <img className="img-sq" src="https://via.placeholder.com/450" alt="BetterHelp logo" />
         </div>
         <div className="project-info">
           <h2>Takeaways</h2>
-          <p><span>Impact:</span> The app makes users feel like Clicked-in really thinks about how to meet their needs.</p>
-          <p><span>What I&rsquo;ve learned:</span> While designing the Clicked-in website, I learned that the first ideas for the app are only the beginning of the process. Usability studies and peer feedback influenced each iteration of the app’s designs.</p>
+          <p><span>Impact:</span> The app makes users feel like they finally got what they wished for, making the overall experience of playing the arcade games way more pleasant.</p>
+          <p><span>What I&rsquo;ve learned:</span> While designing the GameMaster app, I learned that the first ideas for the app are only the beginning of the process. Usability studies and peer feedback influenced each iteration of the app’s designs.</p>
           <h3>Next Steps</h3>
           <p><span>1:</span> Conduct another round of usability studies to validate whether the pain points users experienced have been effectively addressed.</p>
-          <p><span>2:</span>  Conduct more user research to determine any new areas of need.</p>
+          <p><span>2:</span> Conduct more user research to determine any new areas of need.</p>
         </div>
       </div>
     </div>
